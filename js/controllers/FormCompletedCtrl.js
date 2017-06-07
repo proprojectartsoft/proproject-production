b70bc6fe5a27d6bbcc155b7705295db3000e429e
sebastianc_artsoft <sebastian.cocan@artsoft-consult.ro>
2017-06-07 10:02:03 +0300

    share to multiple persons

--- js/controllers/FormCompletedCtrl.js
+++ js/controllers/FormCompletedCtrl.js
@@ -103,7 +103,7 @@ angular.module($APP.name).controller('FormCompletedCtrl', [
             $timeout(function() {
                 navigator.contacts.pickContact(function(contact) {
                     if (contact.emails) {
-                        $scope.filter.email = contact.emails[0].value;
+                        $scope.filter.email = $scope.filter.email + "," + contact.emails[0].value;
                         $timeout(function() {
                             var popup = $ionicPopup.show(createPopup(id));
                         });
