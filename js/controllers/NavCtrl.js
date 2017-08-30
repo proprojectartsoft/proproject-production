806508628b1cd9b2d125767c77602e122f2cd6aa
sebastianc_artsoft <sebastian.cocan@artsoft-consult.ro>
2017-08-30 10:52:43 +0300

    header title

--- js/controllers/NavCtrl.js
+++ js/controllers/NavCtrl.js
@@ -11,23 +11,8 @@ angular.module($APP.name).controller('NavCtrl', [
     'SyncService',
     'DbService',
     function($rootScope, $state, AuthService, $scope, $ionicSideMenuDelegate, CacheFactory, $timeout, $http, $ionicPopup, SyncService, DbService) {
-        //assign the arrow or the burger menu icon when appropriate
-        $scope.$watch(function() {
-            return $('body').attr('class');
-        }, function(cls) {
-            if (cls.indexOf("menu-open") !== -1) {
-                $('.menu-btn button').removeClass("ion-navicon");
-                $('.menu-btn button').addClass("ion-android-arrow-back");
-            } else {
-                $('.menu-btn button').addClass("ion-navicon");
-                $('.menu-btn button').removeClass("ion-android-arrow-back");
-            }
-        });
         $scope.toggleLeft = function($event) {
             $ionicSideMenuDelegate.toggleLeft();
-            $($event.target)
-                .toggleClass("ion-navicon")
-                .toggleClass("ion-android-arrow-back");
         };
         var settingsCache = CacheFactory.get('settings');
         if (!settingsCache) {
